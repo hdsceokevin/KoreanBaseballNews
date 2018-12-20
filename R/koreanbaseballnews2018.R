c4d4dbead71becdae9b8d777ef000e6d1ec9c0dd
MrKevinNa <drkevin2022@gmail.com>
2018-12-20 09:57:19 +0900

    add files

---
+++ R/koreanbaseballnews2018.R
@@ -0,0 +1,7 @@
+#' Korean Baseball News in year of 2018
+#'
+#' @source https://sports.news.naver.com/kbaseball/news/index.nhn?isphoto=N
+#' @format tibble with columns
+#' @examples
+#'   KoreanBaseballNews2018
+'KoreanBaseballNews2018'
